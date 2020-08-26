2f03f41f9f4f55224b1feb0c4734be81c2d6ee9f
mtodorov95 <mtodorov95@gmail.com>
2020-08-26 11:38:05 +0300

    react context api

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,11 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { useStateValue } from "./StateProvider";
 
 function Header() {
+  const [{ basket }] = useStateValue();
+
   return (
     <nav className="header">
       <Link to="/">
@@ -41,7 +44,9 @@ function Header() {
         <Link className="header__link" to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
-            <span className="header__optionLineTwo header__basketCount">0</span>
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
           </div>
         </Link>
       </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,23 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -19,7 +35,9 @@ function Product({ id, title, image, price, rating }) {
         </div>
       </div>
       <img className="product__image" src={image} />
-      <button className="product__btn">Add to basket</button>
+      <button onClick={addToBasket} className="product__btn">
+        Add to basket
+      </button>
     </div>
   );
 }
