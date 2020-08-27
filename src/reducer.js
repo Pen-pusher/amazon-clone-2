681525a8ad670590379861e0c60e9d088361fe3a
mtodorov95 <mtodorov95@gmail.com>
2020-08-27 09:20:05 +0300

    checkout page

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,14 @@
 export const initialState = {
-  basket: [],
+  basket: [
+    {
+      id: "12321341",
+      title:
+        "The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses",
+      price: 11.96,
+      rating: 5,
+      image: "https://m.media-amazon.com/images/I/81-QB7nDh4L._AC_UY218_.jpg",
+    },
+  ],
 };
 
 const reducer = (state, action) => {
